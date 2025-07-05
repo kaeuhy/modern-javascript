d369432104dcc24488c7d40770d3407053952790
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-07-05 10:57:59 +0900

    var, let

---
+++ chapter15/src/ex.js
@@ -0,0 +1,29 @@
+// var x = 1;
+// var y = 1;
+//
+// var x = 100;
+//
+// var y;
+//
+// console.log(x);
+// console.log(y);
+
+// console.log(foo);
+//
+// foo = 123;
+//
+// console.log(foo);
+//
+// var foo;
+
+// var foo = 123;
+// var foo = 456;
+
+// let bar = 123;
+// let bar = 456;
+
+// console.log(foo);
+// console.log(bar);
+
+let foo;
+console.log(foo);
